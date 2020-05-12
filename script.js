624190a541a7d1841cdd9369ab83ee502ac47c1f
Mina G <mghaffar89@gmail.com>
2020-05-12 02:25:42 -0500

    added local storage and save feature

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 var d = new Date();
+//var currentTime = moment().hours();
 document.getElementById("currentDay").innerHTML = d;
 //using moment method
 //moment().format("MMMM Do YYYY, h:mm:ss a");
 //moment(Date);
 //$("#currentDay").text(moment().format("dddd MMMM Do YYYY, h:mm a"));
-//var currentTime = moment();
+
+//Local Storage & Save feature
+var x = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+for (var i = 0; i < x.length; i++) {
+  var dataHour = localStorage.getItem(x[i]);
+  $(".form" + x[i]).val(dataHour);
+}
+$(".saveBtn").click(function () {
+  event.preventDefault();
+  var formValue = $(this).siblings(".form-control").val();
+  console.log("This worked");
+  var listItem = $(this).parent().data("hour");
+
+  localStorage.setItem(listItem, formValue);
+});
